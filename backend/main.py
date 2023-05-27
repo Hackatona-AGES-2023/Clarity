89fe057258710fb87872692612f5a02eebb9a6ed
Gabriel-TabajaraLIS <gabriel.tabajara@pucrs.br>
2023-05-27 14:47:32 -0300

    fixing CORS

--- backend/main.py
+++ backend/main.py
@@ -3,69 +3,40 @@ from http.server import BaseHTTPRequestHandler, HTTPServer
 from api_handler import ApiHandler
 import time
 import json
+from flask import Flask, jsonify, request
+from flask_cors import CORS
+
 
 hostName = "localhost"
 serverPort = 8080
 
-class MyServer(BaseHTTPRequestHandler):
-    def do_GET(self):
-        if self.path == "/":
-            self.send_response(200)
-            self.send_header("Content-type", "text/html")
-            self.end_headers()
-            self.wfile.write(bytes("<html><head><title>https://pythonbasics.org</title></head>", "utf-8"))
-            self.wfile.write(bytes("<p>Request: %s</p>" % self.path, "utf-8"))
-            self.wfile.write(bytes("<p>This is an example web server.</p>", "utf-8"))
-            self.wfile.write(bytes("<body>", "utf-8"))
-            self.wfile.write(bytes("</body></html>", "utf-8"))
-    def do_POST(self):
-        if self.path == "/exec":
-            content_length = int(self.headers['Content-Length'])
-            post_data = self.rfile.read(content_length)
-            data = json.loads(post_data.decode('utf-8'))
+app = Flask(__name__)
+CORS(app)
 
-            if 'message' not in data or 'isGPT' not in data:
-                self.send_response(400)
-                self.send_header('Content-type', 'application/json')
-                self.end_headers()
-                self.wfile.write(b'Missing mandatory attributes.')
-                return
+@app.route('/exec', methods=['POST'])
+def exec_route():
+    if request.method == 'POST':
+        data = request.get_json()
+        if 'message' not in data or 'isGPT' not in data:
+            return jsonify({'error': 'Missing mandatory attributes.'}), 400
 
-            msg = data['message']
-            is_gpt = data['isGPT']
+        msg = data['message']
+        is_gpt = data['isGPT']
 
-            api_handler = ApiHandler()
+        api_handler = ApiHandler()
 
-            response = None 
+        if is_gpt:
+            response = api_handler.chatGPT(msg)
+        else:
+            response = api_handler.soffos(msg)
 
-            if is_gpt:
-                response = api_handler.chatGPT(msg)
-            else:
-                response = api_handler.soffos(msg)
+        response_data = {
+            'message': 'Request received successfully!', 
+            'data': json.loads(response)
+        }
 
-            self.send_response(200)
-            self.send_header('Content-type', 'application/json')
-            self.end_headers()
+        return jsonify(response_data), 200
 
-            # Process the request body
-            request_data = json.loads(response)
 
-            # Prepare the response JSON
-            response_data = {'message': 'Request received successfully!', 'data': request_data}
-
-            response_json = json.dumps(response_data)
-
-            # Send the response
-            self.wfile.write(response_json.encode())
-
-if __name__ == "__main__":        
-    webServer = HTTPServer((hostName, serverPort), MyServer)
-    print("Server started http://%s:%s" % (hostName, serverPort))
-
-    try:
-        webServer.serve_forever()
-    except KeyboardInterrupt:
-        pass
-
-    webServer.server_close()
-    print("Server stopped.")
+if __name__ == '__main__':
+    app.run(host='localhost', port=8080)
