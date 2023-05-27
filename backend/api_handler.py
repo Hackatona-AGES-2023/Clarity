fb405ac7cdc3d91279729d6d8bca208ac33d5931
gRomanini <guilherme.kuticoski@pucrs.br>
2023-05-27 10:21:16 -0300

    request para chatGPT

--- backend/api_handler.py
+++ backend/api_handler.py
@@ -2,8 +2,21 @@ import requests
 
 class ApiHandler:
     def soffos(self, msg):
+        headers = {
+            'Content-Type': 'application/json',
+            'Authorization': 'Bearer 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'
+        }
 
-        print("soffos")
+        body = {
+            'model': 'gpt-3.5-turbo',
+            'messages': [{
+                'role': 'user',
+                'message': msg
+            }]
+        }
+
+        response = requests.post('https://api.soffos.ai/v1/chat', headers=headers, json=body)
+        print(response.json())
 
     def chatGPT(self, msg):
         print("gpt")
