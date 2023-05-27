36dfa8b017bb1f00e4127282bb398c04863127e9
lucasdellatorre <lucasdellatorre222@gmail.com>
2023-05-27 14:27:56 -0300

    remove match

--- backend/main.py
+++ backend/main.py
@@ -5,64 +5,58 @@ import time
 import json
 
 hostName = "localhost"
-serverPort = 3333 
+serverPort = 8080
 
 class MyServer(BaseHTTPRequestHandler):
     def do_GET(self):
-        match self.path:
-            case "/":
-                self.send_response(200)
-                self.send_header("Content-type", "text/html")
-                self.end_headers()
-                self.wfile.write(bytes("<html><head><title>https://pythonbasics.org</title></head>", "utf-8"))
-                self.wfile.write(bytes("<p>Request: %s</p>" % self.path, "utf-8"))
-                self.wfile.write(bytes("<p>This is an example web server.</p>", "utf-8"))
-                self.wfile.write(bytes("<body>", "utf-8"))
-                self.wfile.write(bytes("</body></html>", "utf-8"))
-            case _:
-                print("la ele")
+        if self.path == "/":
+            self.send_response(200)
+            self.send_header("Content-type", "text/html")
+            self.end_headers()
+            self.wfile.write(bytes("<html><head><title>https://pythonbasics.org</title></head>", "utf-8"))
+            self.wfile.write(bytes("<p>Request: %s</p>" % self.path, "utf-8"))
+            self.wfile.write(bytes("<p>This is an example web server.</p>", "utf-8"))
+            self.wfile.write(bytes("<body>", "utf-8"))
+            self.wfile.write(bytes("</body></html>", "utf-8"))
     def do_POST(self):
-        match self.path:
-            case "/exec":
-                content_length = int(self.headers['Content-Length'])
-                post_data = self.rfile.read(content_length)
-                data = json.loads(post_data.decode('utf-8'))
+        if self.path == "/exec":
+            content_length = int(self.headers['Content-Length'])
+            post_data = self.rfile.read(content_length)
+            data = json.loads(post_data.decode('utf-8'))
 
-                if 'message' not in data or 'isGPT' not in data:
-                    self.send_response(400)
-                    self.send_header('Content-type', 'application/json')
-                    self.end_headers()
-                    self.wfile.write(b'Missing mandatory attributes.')
-                    return
-
-                msg = data['message']
-                is_gpt = data['isGPT']
-
-                api_handler = ApiHandler()
-
-                response = None 
-
-                if is_gpt:
-                    response = api_handler.chatGPT(msg)
-                else:
-                    response = api_handler.soffos(msg)
-
-                self.send_response(200)
+            if 'message' not in data or 'isGPT' not in data:
+                self.send_response(400)
                 self.send_header('Content-type', 'application/json')
                 self.end_headers()
+                self.wfile.write(b'Missing mandatory attributes.')
+                return
 
-                # Process the request body
-                request_data = json.loads(response)
+            msg = data['message']
+            is_gpt = data['isGPT']
 
-                # Prepare the response JSON
-                response_data = {'message': 'Request received successfully!', 'data': request_data}
+            api_handler = ApiHandler()
 
-                response_json = json.dumps(response_data)
+            response = None 
 
-                # Send the response
-                self.wfile.write(response_json.encode())
-            case _:
-                print("nothing")
+            if is_gpt:
+                response = api_handler.chatGPT(msg)
+            else:
+                response = api_handler.soffos(msg)
+
+            self.send_response(200)
+            self.send_header('Content-type', 'application/json')
+            self.end_headers()
+
+            # Process the request body
+            request_data = json.loads(response)
+
+            # Prepare the response JSON
+            response_data = {'message': 'Request received successfully!', 'data': request_data}
+
+            response_json = json.dumps(response_data)
+
+            # Send the response
+            self.wfile.write(response_json.encode())
 
 if __name__ == "__main__":        
     webServer = HTTPServer((hostName, serverPort), MyServer)
