75d7e4999a825dc226235edfb681274060f5c4b5
jpfeijo <j.feijo@edu.pucrs.br>
2023-05-27 06:19:46 -0300

    how to use page with header component

--- frontend/src/pages/HowToUsePage/index.tsx
+++ frontend/src/pages/HowToUsePage/index.tsx
@@ -1,3 +1,24 @@
-export default function HowToUsePage() {
+import { Box, Flex } from "@chakra-ui/react";
+import HeaderComponent from "../../components/HeaderComponent";
 
+export default function HowToUsePage() {
+  return (
+    <Box
+      h={'100vh'}
+    >
+      <HeaderComponent />
+      <Flex
+        flexDirection={'column'}
+        justifyContent={'center'}
+        alignItems={'center'}
+        minH={'90%'}
+        w={'100%'}
+        px={'10%'}
+        bg={'#f0f0f0'}
+        gap={'40px'}
+        py={'60px'}
+      >
+      </Flex>
+    </Box>
+  );
 }
